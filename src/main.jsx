f2d25d2c7385998d163153f2d2baf4deb0f40c4c
Vijesh <luckyvijesh@gmail.com>
2024-11-22 19:23:18 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import rStore from './store/rstore.js'
+import rStore from './store/rStore.js'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
